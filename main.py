d961b0859a752c3b01fd3a44f0347522aada3af4
terowoc <terowoc@mail.ru>
2023-12-27 14:22:09 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ api_hash = "7de191651c0201f19bb1451e3b64e5ab"
 
 app = Client("AMinbot", api_id=api_id, api_hash=api_hash)
 
-app.run()
+async def main():
+    async with app:
+        await app.send_message("me", "Hi!")
+
+
+app.run(main())
