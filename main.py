9d9535f0cd412a0f8aa9f75951c6f74df8069f5e
terowoc <terowoc@mail.ru>
2023-12-29 09:08:51 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ api_id = 25836645
 api_hash = "7de191651c0201f19bb1451e3b64e5ab"
 
 app = Client("AMinbot", api_id=api_id, api_hash=api_hash)
-bio = Bio.get_bio()
-
-async def main():
-    async with app:
-        await app.update_profile(bio=bio)
+bio = Bio.get_bio()        
 
 keywords = [
     'oka',
@@ -20,19 +16,16 @@ keywords = [
 
 @app.on_message()
 async def my_handler(client, message):
-    print(message.chat.id)
-    
     await app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+    await app.update_profile(bio=bio)
     
     print(f"Received message: {message.text}")
-    
+
     # Check if the message text contains any keyword from the keywords list
     if any(keyword.lower() in message.text.lower() for keyword in keywords):
         print("Forwarding message...")
         await message.forward("me")
     else:
         print("Message does not match keywords.")
-
-    main()
 
 app.run()
