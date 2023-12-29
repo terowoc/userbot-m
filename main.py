b14fac03e60491998c87eb2899724d13bb0f854b
terowoc <terowoc@mail.ru>
2023-12-29 08:46:24 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -6,8 +6,26 @@ api_hash = "7de191651c0201f19bb1451e3b64e5ab"
 
 app = Client("AMinbot", api_id=api_id, api_hash=api_hash)
 bio = Bio.get_bio()
+
 async def main():
     async with app:
         await app.update_profile(bio=bio)
 
-app.run(main())
+RANDOM = [
+    "Abdullox",
+    'Abu',
+    'Nmagap'
+]
+
+@app.on_message()
+async def my_handler(client, message):
+    print(f"Received message: {message.text}")
+    
+    # Check if the message text contains any keyword from the RANDOM list
+    if any(keyword.lower() in message.text.lower() for keyword in RANDOM):
+        print("Forwarding message...")
+        await message.forward("me")
+    else:
+        print("Message does not match keywords.")
+
+app.run()
